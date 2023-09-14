838b433fb3bb1c6693286e21c01c5e62eb8cf9f8
Siavash Namvar <snsinahub@github.com>
2023-09-14 10:56:22 -0400

    Update demo.cs

--- test/demo.cs
+++ test/demo.cs
@@ -19,8 +19,11 @@ namespace ws.SeleniumTests
 
 
         // create a method to initialize
+        [TestInitialize]
+        public void createDirectory() {
+            Directory.CreateDirectory("/tmp/results");
+        }
         
-
         [ClassInitialize]
         public void Setup()
         {
@@ -54,98 +57,98 @@ namespace ws.SeleniumTests
         
         
 
-        // [TestMethod]
-        // public void TestLink1()
-        // {
-        //     // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-        //     // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
-        //     // test = extent.CreateTest(TestContext.TestName);
-        //     test.Log(Status.Info, "Navigate to DotNet website");
-        //     var driver = GetDriver();
-        //     // using (var driver = GetDriver())
-        //     // {
+        [TestMethod]
+        public void TestLink1()
+        {
+            // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
+            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
+            // test = extent.CreateTest(TestContext.TestName);
+            test.Log(Status.Info, "Navigate to DotNet website");
+            var driver = GetDriver();
+            // using (var driver = GetDriver())
+            // {
                 
-        //         driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
+                driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
 
-        //         // add log from test
+                // add log from test
                 
-        //         //Click the Get Started button
-        //         driver.FindElement(By.LinkText("Privacy Policy")).Click();
-        //         var css = driver.FindElement(By.LinkText("Privacy Policy")).GetAttribute("class");
-        //         var cssMenu = driver.FindElement(By.LinkText("Privacy")).GetAttribute("class");
+                //Click the Get Started button
+                driver.FindElement(By.LinkText("Privacy Policy")).Click();
+                var css = driver.FindElement(By.LinkText("Privacy Policy")).GetAttribute("class");
+                var cssMenu = driver.FindElement(By.LinkText("Privacy")).GetAttribute("class");
 
-        //         Console.WriteLine("CSS ---> " + css);
+                Console.WriteLine("CSS ---> " + css);
                 
-        //         Assert.AreEqual(css, "privacy");
+                Assert.AreEqual(css, "privacy");
                 
-        //     // }
-        // }
+            // }
+        }
         
-        // [TestMethod]
-        // public void TestLink2()
-        // {
-        //     // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-        //     // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
-        //     // using (var driver = GetDriver())
-        //     // {
-        //         var driver = GetDriver();
-        //         test = extent.CreateTest(TestContext.TestName);
-        //         // extent.LogInfo("TestLink2");
-        //         //Navigate to DotNet website
-        //         driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
-        //         //Click the Get Started button
-        //         driver.FindElement(By.LinkText("Privacy")).Click();                
-        //         var cssMenu = driver.FindElement(By.LinkText("Privacy")).GetAttribute("class");
+        [TestMethod]
+        public void TestLink2()
+        {
+            // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
+            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
+            // using (var driver = GetDriver())
+            // {
+                var driver = GetDriver();
+                test = extent.CreateTest(TestContext.TestName);
+                // extent.LogInfo("TestLink2");
+                //Navigate to DotNet website
+                driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
+                //Click the Get Started button
+                driver.FindElement(By.LinkText("Privacy")).Click();                
+                var cssMenu = driver.FindElement(By.LinkText("Privacy")).GetAttribute("class");
 
-        //         Console.WriteLine("CSS MENU ---> " + cssMenu);
+                Console.WriteLine("CSS MENU ---> " + cssMenu);
                 
-        //         Assert.AreNotEqual(cssMenu, "not-privacy");
+                Assert.AreNotEqual(cssMenu, "not-privacy");
     
                 
-        //     // }
-        // }
+            // }
+        }
 
         
 
-        // [TestMethod]
-        // public void TestLink4()
-        // {
-        //     // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-        //     // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
-        //     // using (var driver = GetDriver())
-        //     // {
-        //         var driver = GetDriver();
-        //         test = extent.CreateTest(TestContext.TestName);
-        //         // extent.LogInfo("TestLink2");
-        //         //Navigate to DotNet website
-        //         driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
-        //         //Click the Get Started button
-        //         // test url link if correct after click find element by id
+        [TestMethod]
+        public void TestLink4()
+        {
+            // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
+            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
+            // using (var driver = GetDriver())
+            // {
+                var driver = GetDriver();
+                test = extent.CreateTest(TestContext.TestName);
+                // extent.LogInfo("TestLink2");
+                //Navigate to DotNet website
+                driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
+                //Click the Get Started button
+                // test url link if correct after click find element by id
 
 
-        //         driver.FindElement(By.Id("redirect")).Click();        
-        //         // assert url is correct but do not fail test if not correct
+                driver.FindElement(By.Id("redirect")).Click();        
+                // assert url is correct but do not fail test if not correct
 
-        //         // add a selenium soft assertion to verify url
+                // add a selenium soft assertion to verify url
 
 
 
-        //         // Assert.AreEqual(driver.Url, "https://dotnet.microsoft.com/");    
+                // Assert.AreEqual(driver.Url, "https://dotnet.microsoft.com/");    
 
-        //         // check if driver.url is matching the expected url
-        //         // if not matching, add a soft assertion to the test
-        //         if (driver.Url != "https://dotnet.microsoft.com/")
-        //         {
-        //             test.Log(Status.Warning, "URL is not matching the expected URL");
-        //         } else {
-        //             test.Log(Status.Info, "URL is matching the expected URL");
-        //         }  
+                // check if driver.url is matching the expected url
+                // if not matching, add a soft assertion to the test
+                if (driver.Url != "https://dotnet.microsoft.com/")
+                {
+                    test.Log(Status.Warning, "URL is not matching the expected URL");
+                } else {
+                    test.Log(Status.Info, "URL is matching the expected URL");
+                }  
                 
                 
     
                 
-        //     // }
-        // }
+            // }
+        }
 
         [TestMethod]
         public void TestLink5()
